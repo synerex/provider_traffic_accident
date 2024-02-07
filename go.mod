e7c4290ef2136e62c59087946408397a52d1ff69
Yoshiteru Nagata <yositeru1108@nagoya-u.jp>
2024-02-07 20:14:25 +0900

    minimum sample

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/synerex/provider_traffic_accident
+
+go 1.21
+
+require (
+	github.com/synerex/synerex_proto v0.1.14
+	github.com/synerex/synerex_sxutil v0.7.0
+)
---
+++ traffic-accident.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	pbase "github.com/synerex/synerex_proto"
+	sxutil "github.com/synerex/synerex_sxutil"
+
+	"log"
+	"sync"
+	"time"
+)
+
+var (
+	nodesrv         = flag.String("nodesrv", "127.0.0.1:9990", "Node ID Server")
+	local           = flag.String("local", "", "Local Synerex Server")
+	mu              sync.Mutex
+	version         = "0.0.0"
+	role            = "TrafficAccident"
+	sxServerAddress string
+)
+
+func init() {
+	flag.Parse()
+}
+
+func reconnectClient(client *sxutil.SXServiceClient) {
+	mu.Lock()
+	if client.SXClient != nil {
+		client.SXClient = nil
+		log.Printf("Client reset \n")
+	}
+	mu.Unlock()
+	time.Sleep(5 * time.Second) // wait 5 seconds to reconnect
+	mu.Lock()
+	if client.SXClient == nil {
+		newClt := sxutil.GrpcConnectServer(sxServerAddress)
+		if newClt != nil {
+			log.Printf("Reconnect server [%s]\n", sxServerAddress)
+			client.SXClient = newClt
+		}
+	} else { // someone may connect!
+		log.Printf("Use reconnected server [%s]\n", sxServerAddress)
+	}
+	mu.Unlock()
+}
+
+func main() {
+	go sxutil.HandleSigInt()
+	sxutil.RegisterDeferFunction(sxutil.UnRegisterNode)
+	log.Printf("%s(%s) built %s sha1 %s", role, sxutil.GitVer, sxutil.BuildTime, sxutil.Sha1Ver)
+
+	channelTypes := []uint32{pbase.JSON_DATA_SVC}
+
+	var rerr error
+	sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, role, channelTypes, nil)
+
+	if rerr != nil {
+		log.Fatal("Can't register node:", rerr)
+	}
+	if *local != "" { // quick hack for AWS local network
+		sxServerAddress = *local
+	}
+	log.Printf("Connecting SynerexServer at [%s]", sxServerAddress)
+
+	// wg := sync.WaitGroup{} // for syncing other goroutines
+
+	client := sxutil.GrpcConnectServer(sxServerAddress)
+
+	if client == nil {
+		log.Fatal("Can't connect Synerex Server")
+	} else {
+		log.Print("Connecting SynerexServer")
+	}
+
+	envClient := sxutil.NewSXServiceClient(client, pbase.JSON_DATA_SVC, fmt.Sprintf("{Client:%s}", role))
+
+	smo := sxutil.SupplyOpts{
+		Name: role,
+		JSON: "{\"chincha\": \"chincha\"}", // ここに事故情報を入れる
+	}
+	_, nerr := envClient.NotifySupply(&smo)
+	if nerr != nil {
+		log.Printf("Send Fail! %v\n", nerr)
+	} else {
+		//							log.Printf("Sent OK! %#v\n", ge)
+	}
+
+	// wg.Add(1)
+	// log.Print("Subscribe Supply")
+	// go subscribeJsonRecordSupply(envClient)
+	// wg.Wait()
+}
